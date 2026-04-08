9fb2b77f6943c228c5480e153df3cfb019069641
Jemima Reyes <21041289@itdurango.edu.mx>
2026-04-07 20:19:09 -0600

    added: socket, bind, listen, accept

--- server.s
+++ server.s
@@ -1,4 +1,47 @@
 .intel_syntax noprefix
+
+# socket
+mov rax, 41             # socket syscall code
+mov rdi, 2              # address family ipv4
+mov rsi, 1              # sock stream
+mov rdx, 0              # default protocol
+syscall
+
+mov r8, rax
+
+# bind
+
+mov rcx, 0
+push rcx                # padding
+mov rcx, 0x0000000050000002     # address + port + family
+push rcx
+
+mov rbx, rsp
+
+mov rdi, r8
+mov rsi, rbx
+mov rdx, 0x10           # move fd to for the next call
+mov rax, 49
+syscall
+
+
+# listen
+
+mov rdi, r8
+mov rsi, 0
+mov rax, 50 
+syscall
+
+
+# accept
+mov rdi, r8
+mov rsi, 0
+mov rdx, 0
+mov rax, 43
+syscall
+
+
+# exit
 mov rax, 60
 mov rdi, 0
 syscall
